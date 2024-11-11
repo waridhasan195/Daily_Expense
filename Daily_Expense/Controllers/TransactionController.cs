c1315f2d44656298d0a4257a071298573f1609e4
waridhasan195 <waridhasan195@gmail.com>
2024-11-11 11:40:57 +0600

    Tansection View

---
+++ Daily_Expense/Controllers/TransactionController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Daily_Expense.Data;
+using Daily_Expense.Models;
+
+namespace Daily_Expense.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly DailyExpenseDbContext _context;
+
+        public TransactionController(DailyExpenseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            var dailyExpenseDbContext = _context.Transactions.Include(t => t.Category);
+            return View(await dailyExpenseDbContext.ToListAsync());
+        }
+
+        //// GET: Transaction/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null || _context.Transactions == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var transaction = await _context.Transactions
+        //        .Include(t => t.Category)
+        //        .FirstOrDefaultAsync(m => m.TransactionId == id);
+        //    if (transaction == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(transaction);
+        //}
+
+        // GET: Transaction/Create
+        public IActionResult AddOrEdit()
+        {
+            PopulatedCategory();
+            return View(new Transaction());
+        }
+
+        // POST: Transaction/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amout,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+            return View(transaction);
+        }
+
+        //// GET: Transaction/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.Transactions == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var transaction = await _context.Transactions.FindAsync(id);
+        //    if (transaction == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+        //    return View(transaction);
+        //}
+
+        // POST: Transaction/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("TransactionId,CategoryId,Amout,Note,Date")] Transaction transaction)
+        //{
+        //    if (id != transaction.TransactionId)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(transaction);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!TransactionExists(transaction.TransactionId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+        //    return View(transaction);
+        //}
+
+        // GET: Transaction/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions
+                .Include(t => t.Category)
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'DailyExpenseDbContext.Transactions'  is null.");
+            }
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //private bool TransactionExists(int id)
+        //{
+        //  return (_context.Transactions?.Any(e => e.TransactionId == id)).GetValueOrDefault();
+        //}
+
+        [NonAction]
+        public void PopulatedCategory()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category DefaultCategory = new Category() { CategoryId = 0, Title = "Choose a Category" };
+            CategoryCollection.Insert(0, DefaultCategory);
+            ViewBag.Categories = CategoryCollection;
+        }
+    }
+}
